2a3fba1b7ed0b1dec4ef476bb1a3d4c379368480
Raphael Benitte <benitte@ekino.com>
2016-05-03 11:26:11 +0900

    fix(mini-nav): fix MiniNav component behavior

--- src/components/nav/MiniNav.js
+++ src/components/nav/MiniNav.js
@@ -24,9 +24,9 @@ const miniNavItems = [
     item.absIndex = i;
     if (item.children) {
         item.children = item.children.map((child, childIndex) => {
-            child.index = childIndex + 1;
+            child.index       = childIndex + 1;
             child.parentIndex = item.index;
-            child.absIndex = item.index + childIndex + 1;
+            child.absIndex    = item.index + childIndex + 1;
 
             return child;
         })
@@ -54,11 +54,28 @@ class MiniNav extends Component {
         this.setState({ children: true });
     }
 
+    componentWillReceiveProps(nextProps) {
+        let oldParentPath = '/';
+        if (this.props.routes.length > 1) {
+            oldParentPath += this.props.routes[1].path;
+        }
+
+        let newParentPath = '/';
+        if (nextProps.routes.length > 1) {
+            newParentPath += nextProps.routes[1].path;
+        }
+
+        if (oldParentPath !== newParentPath) {
+            this.setState({ children: true });
+        }
+    }
+
     willEnter(item) {
         const index = item.data.parentIndex !== undefined ? item.data.parentIndex : (item.data.index + 1);
 
         return {
             top:     (index + 1) * 56,
+            scale:   0.6,
             opacity: 0.1,
             zIndex:  10
         };
@@ -69,6 +86,7 @@ class MiniNav extends Component {
 
         return {
             top:     spring((index + 1) * 56, { stiffness: 300, damping: 40 }),
+            scale:   spring(0.6, { stiffness: 300, damping: 40 }),
             opacity: spring(0, { stiffness: 300, damping: 40 }),
             zIndex:  0
         };
@@ -82,7 +100,7 @@ class MiniNav extends Component {
         let childrenItems = [];
 
         if (routes.length > 1) {
-            const parent = _.find(miniNavItems, {path: `/${routes[1].path}`});
+            const parent = _.find(miniNavItems, { path: `/${routes[1].path}`});
             if (parent && parent.children) {
                 hasChildren   = true;
                 currentParent = parent;
@@ -90,7 +108,7 @@ class MiniNav extends Component {
         }
 
         let parentItems = miniNavItems.map(item => {
-            const itemProps = item;
+            const itemProps = _.assign({}, item);
             if (item === currentParent) {
                 itemProps.onClick = this.openChildren;
             }
@@ -101,15 +119,7 @@ class MiniNav extends Component {
         if (hasChildren) {
             childrenItems = [{
                 label: 'back',
-                index: 0,
-                element: (
-                    <span
-                        className="mini-nav_item mini-nav_item-back"
-                    >
-                        <span className="mini-nav_item_feedback" />
-                        <span className="mini-nav_item_icon" />
-                    </span>
-                )
+                index: 0
             }].concat(currentParent.children);
         }
 
@@ -126,10 +136,7 @@ class MiniNav extends Component {
 
         return (
             <aside className="mini-nav">
-                <Link
-                    className="mini-nav_item mini-nav_item-nivo"
-                    to="/"
-                >
+                <Link className="mini-nav_item mini-nav_item-nivo" to="/">
                     <span className="mini-nav_item_icon" />
                 </Link>
                 <TransitionMotion
@@ -141,7 +148,9 @@ class MiniNav extends Component {
                             data:  item,
                             style: {
                                 top:     spring((item.index + 1) * 56, { stiffness: 120, damping: 11 }),
-                                opacity: spring(1)
+                                scale:   spring(1, { stiffness: 120, damping: 11 }),
+                                opacity: spring(1),
+                                zIndex:  10,
                             }
                         };
                     })}
@@ -151,16 +160,16 @@ class MiniNav extends Component {
                             {interpolatedStyles.map(item => {
                                 const style = {
                                     opacity:   item.style.opacity,
-                                    transform: `translate3d(0,${item.style.top}px,0)`,
+                                    transform: `translate3d(0,${item.style.top}px,0) scale(${item.style.scale})`,
                                     zIndex:    item.style.zIndex
                                 };
 
                                 if (item.data.path) {
-                                    return <MiniNavLink style={style} {...item.data} />;
+                                    return <MiniNavLink key={item.key} style={style} {...item.data} />;
                                 }
 
                                 return (
-                                    <span onClick={this.closeChildren} className="mini-nav_item mini-nav_item-back" style={style}>
+                                    <span key="back" onClick={this.closeChildren} className="mini-nav_item mini-nav_item-back" style={style}>
                                         <span className="mini-nav_item_icon" />
                                     </span>
                                 );
